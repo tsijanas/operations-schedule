8119184b153efe0c205d15088ae22e11b27e0a5a
tsijanas <90253520+tsijanas@users.noreply.github.com>
2026-07-17 11:49:21 +0300

    Add files via upload

---
+++ background.js
@@ -0,0 +1,15 @@
+{
+  "manifest_version": 3,
+  "name": "Rota Scheduler Launcher",
+  "version": "1.0",
+  "description": "Quick access to the team Rota platform.",
+  "background": {
+    "service_worker": "background.js"
+  },
+  "action": {
+    "default_icon": "icon.png"
+  },
+  "permissions": [
+    "tabs"
+  ]
+}
